73816597deb94f4b1da92af199b1c63b6c711631
Ella <you@example.com>
2023-02-17 21:12:42 +0300

    metod

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,11 @@
-public class Main {
+    public class Main {
+
     public static void main(String[] args) {
-        int account = 100;
-        int deposit = 1200;
-        int bonus;
-        if (deposit > 1000) {
-            bonus = deposit / 100;
-        } else {
-            bonus = 0;
-        }
-        int totalAmount = account + deposit + bonus;
-        System.out.println("Итоговая сумма на счету:");
-        System.out.println(totalAmount);
-        System.out.println("В том числе бонусные рубли за пополнение:");
-        System.out.println(bonus);
+        BonusMilesService service = new BonusMilesService();
+        int price = 10_000;
+        int miles = service.calculate(price);
+        System.out.println(miles);
     }
 }
+
 
